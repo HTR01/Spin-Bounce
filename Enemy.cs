43e21ac3cf4cab2953b3ed312debef1667291f90
HTR01 <54290778+HTR01@users.noreply.github.com>
2019-09-03 09:24:30 +1200

    Add files via upload
    Change the co-ordinates to values needed
    Change speed to value needed
    Repeat Instantiate along all 4 sides

---
+++ Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float movementSpeed = 500;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, -634, 0), movementSpeed * Time.deltaTime);
+    }
+}
---
+++ RandomSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public GameObject bullet;
+    int randomNumber;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandom", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRandom()
+    {
+        Instantiate(bullet, new Vector3(Random.Range(-1190, 1190), 750, 0), transform.rotation);
+
+
+    }
+}
